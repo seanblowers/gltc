4d3ac42cff501fbdcfb896b7a82a1900a6d2f016
seanblowers <sean@crtlly.com>
2025-02-22 16:58:32 -0600

    dfdsf

--- script.js
+++ script.js
@@ -66,15 +66,4 @@ function reloadVideo(type) {
 
 // Fullscreen Toggle
 function toggleFullscreen() {
-    const videoContainer = document.querySelector('.video-container');
-    if (!document.fullscreenElement) {
-        videoContainer.requestFullscreen().catch(err => {
-            alert('Error attempting to enable fullscreen mode.');
-        });
-    } else {
-        document.exitFullscreen();
-    }
-}
-
-// Initialize Default View
-showView('home-view');
+    const videoContainer = document.querySelector
